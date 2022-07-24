e2f5e58409d39c4ca0dd6631b9d60c4b0f5195b0
Srayan Jana <valorzard.kiko@outlook.com>
2022-07-24 15:11:32 -0700

    refactor row spawn code

--- Level/Level.gd
+++ Level/Level.gd
@@ -15,29 +15,36 @@ var brick_template : PackedScene = preload("res://Level/Brick.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# spawn initial row
-	var i := 0
-	while(i < num_of_blocks_in_row):
-		var brick := brick_template.instance()
-		$Bricks.add_child(brick)
-		brick.position = Vector2(space_between_bricks * i, 0)
-		brick.row = current_row
-		i += 1
+	_spawn_row()
 	
 	# if the ball hit the ground, spawn next row
-	GameManager.connect("hit_ground", self, "_spawn_next_row")
+	GameManager.connect("hit_ground", self, "_setup_next_round")
 
-func _spawn_next_row():
+func _generate_row_array() -> Array:
+	var i := 0
+	var row_array : Array = Array()
+	while(i < num_of_blocks_in_row):
+		row_array.append(true)
+		i += 1
+	return row_array
+
+func _setup_next_round():
 	# move all the Bricks down
 	for brick in $Bricks.get_children():
 		brick.position.y += space_between_rows
 	
+	_spawn_row()
+
+func _spawn_row():
 	# spawn new row
+	var row_array := _generate_row_array()
 	var i := 0
-	while(i < num_of_blocks_in_row):
-		var brick := brick_template.instance()
-		$Bricks.add_child(brick)
-		brick.position = Vector2(space_between_bricks * i, 0)
-		brick.row = current_row
+	for brick_pos in row_array:
+		if brick_pos == true:
+			var brick := brick_template.instance()
+			$Bricks.add_child(brick)
+			brick.position = Vector2(space_between_bricks * i, 0)
+			brick.row = current_row
 		i += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
